21def247aca03191a903b5ff4c0e42af611ef368
Swiffer96 <jpy2gtjbm5@privaterelay.appleid.com>
2026-06-27 13:50:05 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'coach-sportif-v4-cache';
+const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
